ee8ce2c3807ba4c98d77c06fd938d465fe3b8302
BondAlexander <alexancb@rams.colostate.edu>
2022-02-28 13:29:06 -0700

    Added CST_IMC file and updated requirements.txt

--- python_zeros.py
+++ python_zeros.py
@@ -35,68 +35,71 @@ def to_doc_w(file_name, varable):
     f.write(varable)
     f.close()
 
+def main():
+    with open('/home/gregorya/zeros/completed_devices_file') as f:
 
-with open('/home/gregorya/zeros/completed_devices_file') as f:
+        devices_list = f.read().splitlines()
 
-    devices_list = f.read().splitlines()
+    start = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
 
-start = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
-
-print('Begin operation - ' + start)
-
-
-file_name = datetime.datetime.now().strftime("%Y%m%d-%H%M")
-
-to_doc_w(file_name, "")
+    print('Begin operation - ' + start)
 
 
+    file_name = datetime.datetime.now().strftime("%Y%m%d-%H%M")
 
-for devices in devices_list:
-    print('Connecting to device ' + devices)
-    ip_address_of_device = devices
-    hp_devices = {
-        'device_type': 'hp_procurve',
-        'ip': ip_address_of_device, 
-        'username': username,
-        'password': password,
-        'global_delay_factor': .25
-    }
-
-    try:
-        net_connect = ConnectHandler(**hp_devices)
-    except (AuthenticationException):
-        print ('********************Authentication failure: ' + ip_address_of_device)
-        continue
-    except (NetMikoTimeoutException):
-        print ('-------------------Timeout to device: ' + ip_address_of_device)
-        continue
-    except (EOFError):
-        print ("End of file while attempting device " + ip_address_of_device)
-        continue
-    except (SSHException):
-        print ('SSH Issue. Are you sure SSH is enabled? ' + ip_address_of_device)
-        continue
-    except Exception as unknown_error:
-        print ('Some other error: ' + str(unknown_error))
-        continue
+    to_doc_w(file_name, "")
 
 
-    sysoutput = net_connect.send_command_expect('show system', expect_string=r">")
-    intoutput = net_connect.send_command_expect('show interface', expect_string=r">")
-    linebreak = "*-*-*-*-" * 15
-    #print(sysoutput)
-    #print(intoutput)
-    #print(linebreak)
+
+    for devices in devices_list:
+        print('Connecting to device ' + devices)
+        ip_address_of_device = devices
+        hp_devices = {
+            'device_type': 'hp_procurve',
+            'ip': ip_address_of_device, 
+            'username': username,
+            'password': password,
+            'global_delay_factor': .25
+        }
+
+        try:
+            net_connect = ConnectHandler(**hp_devices)
+        except (AuthenticationException):
+            print ('********************Authentication failure: ' + ip_address_of_device)
+            continue
+        except (NetMikoTimeoutException):
+            print ('-------------------Timeout to device: ' + ip_address_of_device)
+            continue
+        except (EOFError):
+            print ("End of file while attempting device " + ip_address_of_device)
+            continue
+        except (SSHException):
+            print ('SSH Issue. Are you sure SSH is enabled? ' + ip_address_of_device)
+            continue
+        except Exception as unknown_error:
+            print ('Some other error: ' + str(unknown_error))
+            continue
+
+
+        sysoutput = net_connect.send_command_expect('show system', expect_string=r">")
+        intoutput = net_connect.send_command_expect('show interface', expect_string=r">")
+        linebreak = "*-*-*-*-" * 15
+        #print(sysoutput)
+        #print(intoutput)
+        #print(linebreak)
+        finish = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
+        print('Operation Complete - ' + finish)
+        print('\n' * 1)
+        #Append the output to the results file
+        to_doc_a(file_name, devices)
+        to_doc_a(file_name, sysoutput)
+        to_doc_a(file_name, intoutput)
+        to_doc_a(file_name, linebreak)
+        to_doc_a(file_name, finish)
+
+
     finish = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
     print('Operation Complete - ' + finish)
-    print('\n' * 1)
-    #Append the output to the results file
-    to_doc_a(file_name, devices)
-    to_doc_a(file_name, sysoutput)
-    to_doc_a(file_name, intoutput)
-    to_doc_a(file_name, linebreak)
-    to_doc_a(file_name, finish)
 
-
-finish = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
-print('Operation Complete - ' + finish)
+if __name__ == '__main__':
+    main()
